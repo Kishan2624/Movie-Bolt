54dc2e0eee0113feb9abe11d43580a2b0128c58b
kishan kumar <ftd10822422@dseu.ac.in>
2024-07-14 19:07:43 +0530

    auth 0

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -11,6 +11,7 @@ const Header = () => {
   
 
   if (isLoading) {
+    console.log(user)
     return <div>Loading ...</div>;
   }
   
--- src/main.jsx
+++ src/main.jsx
@@ -18,7 +18,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Auth0Provider
     domain="dev-5ycylhzm2k5vh7ai.us.auth0.com"
-    clientId="tCcnhYP7iKbtm92MM7AueuAuC5KgYBp6"
+    clientId="uEdWccuqE618VUe6NLjkb1KyBBYdo3KT"
     authorizationParams={{
       redirect_uri: window.location.origin
     }}
