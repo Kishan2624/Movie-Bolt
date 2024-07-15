e6ce713ed806f14d21740adee9ffe432442bf36d
kishan kumar <ftd10822422@dseu.ac.in>
2024-07-15 17:04:21 +0530

    fix width ifram

--- src/component/VideoPlay.jsx
+++ src/component/VideoPlay.jsx
@@ -6,9 +6,9 @@ const VideoPlay = ({ data,close, media_type}) => {
     const {data : videoData} = useFetchDetails(`/${media_type}/${data.id}/videos`)
   return (
     <section className="fixed bg-neutral-700 top-0 bottom-0 left-0 z-40 bg-opacity-60 w-full flex justify-center items-center">
-      <div className="bg-black w-full max-h-[80vh] max-w-screen-lg aspect-video rounded relative">
+      <div className="bg-black w-full max-h-[80vh] max-w-[90%]  lg:max-w-screen-lg aspect-video rounded relative">
         <button className="absolute -right-1 -top-8 text-4xl " onClick={close}><IoCloseSharp /></button>
-        <iframe src={`https://www.youtube.com/embed/${videoData?.results[0]?.key}`} className={"w-full h-full"} frameborder="0"/>
+        <iframe src={`https://www.youtube.com/embed/${videoData?.results[0]?.key}`} className={"w-full h-full rounded"} frameborder="0"/>
       </div>
 
     </section>
